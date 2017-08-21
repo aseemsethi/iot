be7b40512334be21e697d921fa2374254916cc77
asethi <asethi@prysm.com>
2017-08-21 14:30:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/asethi/mqttapp/SearchActivity.java
@@ -0,0 +1,202 @@
+package com.asethi.mqttapp;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.Formatter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import static android.R.attr.value;
+import static android.net.ConnectivityManager.TYPE_MOBILE;
+import static android.net.ConnectivityManager.TYPE_WIFI;
+
+/**
+ * Created by ASethi on 20-08-2017.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    TextView ipaddressF, hostsF;
+    private ProgressBar progress;
+    String selfIP = "0.0.0.0";
+    String subnetScan = "192.168.1.";
+    ArrayList<String> subnetList;
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Use the following 2 lines - else use async threads
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        setContentView(R.layout.search_activity);
+        ipaddressF = (TextView) findViewById(R.id.ipaddress);
+        hostsF = (TextView) findViewById(R.id.hosts);
+        ipaddressF.setText(null);
+        progress = (ProgressBar) findViewById(R.id.progressBar1);
+        //make the progress bar visible
+        progress.setVisibility(View.VISIBLE);
+
+        // Initialize the self-ip and subnet scan to relevant ip addresses.
+        getIP(getApplicationContext());
+        EditText inputTxt = (EditText) findViewById(R.id.subnetScanUI);
+        inputTxt.setText(selfIP.substring(0, selfIP.lastIndexOf('.')+1));
+        System.out.println("Aseem:" + selfIP.substring(0, selfIP.lastIndexOf('.')+1));
+        subnetScan = selfIP.substring(0, selfIP.lastIndexOf('.')+1);
+
+        Button search = (Button) findViewById(R.id.searchStartB);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+                EditText inputTxt = (EditText) findViewById(R.id.subnetScanUI);
+                String typedText = inputTxt.getText().toString();
+                subnetScan = typedText;
+                EditText inputTxt1 = (EditText) findViewById(R.id.subnetRangeUI);
+                final int subnetRange = Integer.parseInt( inputTxt1.getText().toString() );
+
+                boolean cont = getIP(getApplicationContext());
+                if (cont == false) return;
+                Runnable runnable = new Runnable() {
+                    @Override public void run() {
+                        subnetList = scanSubNet(subnetScan, subnetRange);
+                    }
+                };
+                new Thread(runnable).start();
+            }
+        });
+        // Wait till thread above completes
+        // hostsF.setText(subnetList.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean getIP(Context context) {
+        int[] networkTypes = {TYPE_MOBILE,
+                TYPE_WIFI};
+        try {
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            for (int networkType : networkTypes) {
+                NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                if (activeNetworkInfo != null &&
+                        activeNetworkInfo.getType() == networkType) {
+                    Toast.makeText(context, connectivityManager.getActiveNetworkInfo().getTypeName(), Toast.LENGTH_LONG).show();
+                    System.out.println("Connection State: " + connectivityManager.getActiveNetworkInfo().getDetailedState()); // Connected ?
+                    System.out.println("SSID: " + connectivityManager.getActiveNetworkInfo().getExtraInfo()); // SSID
+                    System.out.println("TypeName: " + connectivityManager.getActiveNetworkInfo().getTypeName()); //WiFi
+                }
+            }
+            if (connectivityManager.getActiveNetworkInfo().getTypeName().contains("WIFI")) {
+                WifiManager myWifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
+                WifiInfo myWifiInfo = myWifiManager.getConnectionInfo();
+                int ipAddress = myWifiInfo.getIpAddress();
+                System.out.println("WiFi address: " + Formatter.formatIpAddress(ipAddress));
+                String ip = String.format("%d.%d.%d.%d", (ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff));
+                System.out.println("WiFi address: " + ip);
+                ipaddressF.setText(ip);
+                selfIP = ip;
+                return true;
+            } else {
+                ipaddressF.setText("0.0.0.0");
+                Toast.makeText(context, "Need WiFi Connection", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static boolean isReachable(String addr, int openPort, int timeOutMillis) {
+        // Any Open port on other machine
+        // openPort =  22 - ssh, 80 or 443 - webserver, 25 - mailserver etc.
+        try {
+            Socket soc = new Socket();
+            soc.connect(new InetSocketAddress(addr, openPort), timeOutMillis);
+            System.out.println("Reachable addr: " + addr);
+            return true;
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+
+    private ArrayList<String> scanSubNet(String subnet, int subnetRange){
+        final ArrayList<String> hosts = new ArrayList<String>();
+        InetAddress inetAddress = null;
+        progress.setMax(subnetRange);
+        for(int i=1; i<=subnetRange; i++) {
+            final int val = i;
+            System.out.println("Trying: " + subnet + String.valueOf(i));
+            mHandler.post(new Runnable() {  // or progress.post
+                @Override
+                public void run() {
+                    System.out.println("val: " + val);
+                    progress.setProgress(val);
+                    if (hosts != null)
+                        hostsF.setText(hosts.toString());
+                }
+            });
+            try {
+                inetAddress = InetAddress.getByName(subnet + String.valueOf(i));
+                if (inetAddress.isReachable(1000)) {
+                    hosts.add(inetAddress.getHostName());
+                    System.out.println("Found Device using Ping: " + inetAddress.getHostName());
+                } else if (isReachable(subnet + String.valueOf(i), 80, 1000)) {
+                    hosts.add(inetAddress.getHostName());
+                    System.out.println("Found Device using Telnet:80: " + inetAddress.getHostName());
+                } else if (isReachable(subnet + String.valueOf(i), 22, 1000)) {
+                    hosts.add(inetAddress.getHostName());
+                    System.out.println("Found Device using SSH:80: " + inetAddress.getHostName());
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return hosts;
+    }
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
---
+++ app/src/main/java/com/asethi/mqttapp/ToolActivity.java
@@ -0,0 +1,250 @@
+package com.asethi.mqttapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class ToolActivity extends AppCompatActivity {
+
+    private HistoryAdapter mAdapter;
+    private MqttAndroidClient mqttAndroidClient;
+    // final String serverUri = "test.mosquitto.org:1883";
+    String serverUri = "tcp://iot.eclipse.org:1883";
+    String clientId = "AndroidClient";
+    String subscriptionTopic = "Topic1";
+    String publishTopic = "Topic1";
+    final String publishMessage = "Hello World !";
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tool_activity);
+
+        Button pub = (Button) findViewById(R.id.publishB);
+        pub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishMessage();
+            }
+        });
+
+        Button conn = (Button) findViewById(R.id.connectB);
+        conn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText inputTxt = (EditText) findViewById(R.id.mqtt_server);
+                String typedText = inputTxt.getText().toString();
+                serverUri = typedText;
+                EditText inputTopicP = (EditText) findViewById(R.id.mqtt_topicP);
+                String typedTopicP = inputTopicP.getText().toString();
+                publishTopic = typedTopicP;
+                connectToServer();
+            }
+        });
+
+        Button disconn = (Button) findViewById(R.id.disconnectB);
+        disconn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if(mqttAndroidClient != null)
+                        mqttAndroidClient.disconnect();
+                        mqttAndroidClient = null;
+                        addToHistory("Disconnecting from : " + serverUri);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Button subs = (Button) findViewById(R.id.subscribeB);
+        subs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                subscribeToTopic();
+            }
+        });
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.history_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new HistoryAdapter(new ArrayList<String>());
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    public void connectToServer() {
+        // If the client id is not unique, the earlier subscription in the DB file is used
+        // and the client recvd messages not subscribed to in the callback below
+        clientId = clientId + System.currentTimeMillis();
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String s) {
+                System.out.println("Connection completed: ");
+                if (reconnect) {
+                    addToHistory("Reconnected to : " + serverUri);
+                    // Because Clean Session is true, we need to re-subscribe
+                    //subscribeToTopic();
+                } else {
+                    addToHistory("Connected to: " + serverUri);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+                System.out.println("Connection lost: ");
+                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                System.out.println("Incoming Msg...: " + new String(mqttMessage.getPayload()));
+                addToHistory("Incoming message...: " + new String(mqttMessage.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken iMqttToken) {
+                    System.out.println("Connected to : " + serverUri);
+                    addToHistory("Connected to: " + serverUri);
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    //subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                    System.err.println("Failed to connect to : " + serverUri);
+                    addToHistory("Failed to connect to: " + serverUri);
+                }
+            });
+        } catch (MqttException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void addToHistory(String mainText){
+        String format = simpleDateFormat.format(System.currentTimeMillis());
+
+        System.out.println("LOG: " + mainText);
+        mAdapter.add(format + ": " + mainText);
+        Snackbar.make(findViewById(android.R.id.content), mainText, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+    }
+
+    public void subscribeToTopic() {
+        EditText inputTopicS = (EditText) findViewById(R.id.mqtt_topicS);
+        String typedTopicS = inputTopicS.getText().toString();
+        subscriptionTopic = typedTopicS;
+
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken iMqttToken) {
+                    System.out.println("Successfully subscribed to topic : " + subscriptionTopic);
+                    addToHistory("Subscribed to : " + subscriptionTopic);
+                }
+
+                @Override
+                public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                    System.err.println("UnSuccessfully subscribed to topic : " + subscriptionTopic);
+                    addToHistory("Subscription Failure: " + subscriptionTopic);
+                }
+            });
+
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                    String msg = new String(mqttMessage.getPayload());
+                    addToHistory("Message Recvd: " + s + ":" + msg);
+                    System.out.println("Message Recvd on Topic: " + s + ":" + msg);
+                }
+            });
+        } catch (MqttException ex) {
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage() {
+        try {
+            if (mqttAndroidClient == null) {
+                addToHistory("Not connected to server");
+                return;
+            }
+            System.out.println("Publishing Message: ");
+            addToHistory("Publishing Message: " + publishMessage);
+            MqttMessage message = new MqttMessage();
+            message.setPayload(publishMessage.getBytes());
+            mqttAndroidClient.publish(publishTopic, message);
+
+        } catch (MqttException e) {
+            System.err.println("Error publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
